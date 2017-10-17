ba79c7b9bc7b1a2e329beb2578fb606955ab0817
İbrahim Ş. Örencik <o@yedincisenol.com>
2017-10-17 15:59:28 +0300

    Added order details

--- catalog/controller/api/custom.php
+++ catalog/controller/api/custom.php
@@ -44,9 +44,11 @@ class ControllerApiCustom extends Controller
     {
         if ($this->auth()) {
             $order  =   new ModelSaleOrder($this->registry);
-            $orders =   $order->getOrders($_GET);
+            $orders =   $this->getOrders($_GET);
             $orders =   $this->paginate($orders);
             $orders =   array_map(function($aorder) use ($order) {
+                $aorder['custom_field'] =   unserialize($order->custom_field);
+                $aorder['payment_custom_field'] =   unserialize($order->payment_custom_field);
                 $aorder['products'] =   $order->getOrderProducts($aorder['order_id']);
                 $aorder['totals']   =   $order->getOrderTotals($aorder['order_id']);
                 return $aorder;
@@ -104,6 +106,86 @@ class ControllerApiCustom extends Controller
         $this->data['meta']['pagination'] = $paginate;
 
         return array_slice($results, ($page - 1) * $limit, $limit);
+    }
+
+    private function getOrders($data = array()) {
+
+        $sql = "SELECT o.*, (SELECT os.name FROM " . DB_PREFIX . "order_status os WHERE os.order_status_id = o.order_status_id AND os.language_id = '" . (int)$this->config->get('config_language_id') . "') AS status, o.shipping_code, o.total, o.currency_code, o.currency_value, o.date_added, o.date_modified FROM `" . DB_PREFIX . "order` o";
+
+        if (isset($data['filter_order_status'])) {
+            $implode = array();
+
+            $order_statuses = explode(',', $data['filter_order_status']);
+
+            foreach ($order_statuses as $order_status_id) {
+                $implode[] = "o.order_status_id = '" . (int)$order_status_id . "'";
+            }
+
+            if ($implode) {
+                $sql .= " WHERE (" . implode(" OR ", $implode) . ")";
+            } else {
+
+            }
+        } else {
+            $sql .= " WHERE o.order_status_id > '0'";
+        }
+
+        if (!empty($data['filter_order_id'])) {
+            $sql .= " AND o.order_id = '" . (int)$data['filter_order_id'] . "'";
+        }
+
+        if (!empty($data['filter_customer'])) {
+            $sql .= " AND CONCAT(o.firstname, ' ', o.lastname) LIKE '%" . $this->db->escape($data['filter_customer']) . "%'";
+        }
+
+        if (!empty($data['filter_date_added'])) {
+            $sql .= " AND DATE(o.date_added) = DATE('" . $this->db->escape($data['filter_date_added']) . "')";
+        }
+
+        if (!empty($data['filter_date_modified'])) {
+            $sql .= " AND DATE(o.date_modified) = DATE('" . $this->db->escape($data['filter_date_modified']) . "')";
+        }
+
+        if (!empty($data['filter_total'])) {
+            $sql .= " AND o.total = '" . (float)$data['filter_total'] . "'";
+        }
+
+        $sort_data = array(
+            'o.order_id',
+            'customer',
+            'status',
+            'o.date_added',
+            'o.date_modified',
+            'o.total'
+        );
+
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            $sql .= " ORDER BY " . $data['sort'];
+        } else {
+            $sql .= " ORDER BY o.order_id";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC";
+        } else {
+            $sql .= " ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+
+        $query = $this->db->query($sql);
+
+        return $query->rows;
     }
 
     /**
